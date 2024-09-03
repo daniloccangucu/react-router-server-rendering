8e0ff7b2000932c37508a13968dffd6723aa21ca
Danilo Canguçu <daniloccangucu@gmail.com>
2024-09-03 15:49:04 +0200

    server/index.js refactoring

--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,57 @@
-import express from "express";
-import cors from "cors";
 import ReactDOM from "react-dom/server";
 import * as React from "react";
+import express from "express";
+import cors from "cors";
+import serialize from "serialize-javascript";
+import { matchPath } from "react-router-dom";
+import { StaticRouter } from "react-router-dom/server";
+
 import App from "../shared/App";
+import routes from "../shared/routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.static("dist"));
 
-const PORT = process.env.PORT || 3000;
-
 app.get("*", (req, res, next) => {
-  const markup = ReactDOM.renderToString(<App />);
-  res.send(`
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>SSR with React Router</title>
-                <script src="/bundle.js" defer></script>    
-                <link href="/main.css" rel="stylesheet">
-            </head>
+  const activeRoute =
+    routes.find((route) => matchPath(route.path, req.url)) || {};
 
-            <body>
-                <div id="app">${markup}</div>
-            </body>
-        </html>
-        `);
+  const promise = activeRoute.fetchInitialData
+    ? activeRoute.fetchInitialData(req.path)
+    : Promise.resolve(); // undefined
+
+  promise
+    .then((data) => {
+      const markup = ReactDOM.renderToString(
+        <StaticRouter location={req.url}>
+          <App serverData={data} />
+        </StaticRouter>
+      );
+
+      res.send(`
+      <!DOCTYPE html>
+      <html>
+          <head>
+              <title>SSR with React Router</title>
+              <script src="/bundle.js" defer></script>    
+              <link href="/main.css" rel="stylesheet">
+              <script>
+                window.__INITIAL_DATA__ = ${serialize(data)}
+              </script>
+          </head>
+
+          <body>
+              <div id="app">${markup}</div>
+          </body>
+      </html>
+      `);
+    })
+    .catch(next);
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);

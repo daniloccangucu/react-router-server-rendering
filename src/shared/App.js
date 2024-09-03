89bc5cd84ba965711e851f4dea75a5018bb97d5d
Danilo Canguçu <daniloccangucu@gmail.com>
2024-09-03 15:50:08 +0200

    App component refactoring

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import * as React from "react";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-  return <div>Hello world</div>;
+import "./styles.css";
+import ColorfulBorder from "./ColorfulBorder";
+import NavBar from "./NavBar";
+import NoMatch from "./NoMatch";
+import routes from "./routes";
+
+export default function App({ serverData = null }) {
+  return (
+    <React.Fragment>
+      <ColorfulBorder />
+      <div className="container">
+        <NavBar />
+        <Routes>
+          {routes.map(({ path, fetchInitialData, component: C }) => {
+            return (
+              <Route
+                key={path}
+                path={path}
+                element={
+                  <C data={serverData} fetchInitialData={fetchInitialData} />
+                }
+              />
+            );
+          })}
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </div>
+    </React.Fragment>
+  );
 }
-
-export default App;

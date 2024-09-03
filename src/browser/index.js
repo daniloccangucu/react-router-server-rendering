9f12983dfadd47344897a95114bd5f41f1a48f1c
Danilo Canguçu <daniloccangucu@gmail.com>
2024-09-03 15:48:23 +0200

    browser/index.js wrapped in BrowserRouter

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,6 +1,12 @@
 import * as React from "react";
 import { hydrateRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "../shared/App";
 
-const container = document.getElementById("app");
-hydrateRoot(container, <App />);
+hydrateRoot(
+  document.getElementById("app"),
+  <BrowserRouter>
+    <App serverData={window.__INITIAL_DATA__} />
+  </BrowserRouter>
+);
